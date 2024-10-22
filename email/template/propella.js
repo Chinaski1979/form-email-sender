b443483e84b97555f47038977adeb52a6882f83a
josemorales-g360 <Jose_Morales@cw.groups360.com>
2024-10-21 21:12:36 -0600

    refactor existing logic and using commonjs

--- email/template/propella.js
+++ email/template/propella.js
@@ -1,25 +1,17 @@
-import { cleanText } from "../../common/textUtils";
-
-const { FORM_NAME_KEY } = require("../../common/variables");
-
-export const propellaCommonTemplate = (reqBody) => {
-  const fields = Object.entries(reqBody)
-
+const propellaCommonTemplate = (fields) => {
   const list = fields
-    .filter(([key]) => key !== FORM_NAME_KEY)
     .map(([key, value]) => {
-    const buffer = Buffer.from(key, 'latin1');
-    const correctedText = buffer.toString('utf8');
-    return (
-      `<li> 
-        <b>
-          ${cleanText(correctedText)}: 
-        </b>
-        ${value}
-      </li>`
-    )
-  })
+      return (
+        `<li> 
+          <b>
+            ${key}: 
+          </b>
+          ${value}
+        </li>`
+      );
+    });
 
-  return `<ul>${list.join('')}</ul>`
+  return `<ul>${list.join('')}</ul>`;
+};
 
-}
+module.exports = { propellaCommonTemplate };
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
 const propella = require('./propella.route');
+const mobula = require('./mobula.route');
 
 const r = Router();
 
 r.use('/demo', demo);
 r.use('/propella', propella);
+r.use('/mobula', mobula);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
