b88dcef421d0cb6fb7f78583937cfb420f8a7394
josemorales-g360 <Jose_Morales@cw.groups360.com>
2025-02-19 19:56:19 -0600

    add planner app support

---
+++ email/template/planner-app.js
@@ -0,0 +1,19 @@
+const plannerAppCommonTemplate = (reqBody) => {
+    const fields = Object.entries(reqBody);
+
+    const list = fields
+        .map(([key, value]) => {
+            return (
+                `<li> 
+            <b>
+              ${key}: 
+            </b>
+            ${value}
+          </li>`
+            );
+        });
+
+    return `<ul>${list.join('')}</ul>`;
+};
+
+module.exports = { plannerAppCommonTemplate };
