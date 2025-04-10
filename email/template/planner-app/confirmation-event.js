456b14a7727f61b2af66e099d3f6c32ddf49382c
josemorales-g360 <Jose_Morales@cw.groups360.com>
2025-04-10 16:15:37 -0600

    update template and add a new template

--- email/template/planner-app/confirmation-event.js
+++ email/template/planner-app/confirmation-event.js
@@ -1,29 +1,32 @@
 
 const confirmationEventTemplate = (item) => {
   return (
-    `<h3 style='color:black'>
-            Hi ${item?.clientName}
-            </h3>
-        <p  style='color:black'>Your event has been confirmed for: ${item?.startDate} at ${item?.startTime} , in the location: <b style="color:teal">${item?.locationName}</b></p>
-          <h5>
-            <b style="color:teal">Service Information:</b>
-          </h5>
-          <ul style='color:black'>
-            <li>FAX#: ${item?.headcount?.toddlers + item?.headcount?.adults}</li>
-            <li>Location: ${item?.locationName}</li>
-            <li>Date: ${item?.startDate}</li>
-            <li>Menu Items: ${item?.menuName}</li>
-            <li>Staff: ${item?.staff}</li>
-          </ul>
-          <h5>
-            <b style="color:teal">Client Information:</b>
-          </h5>
-          <ul style='color:black'>
-            <li>Client Name: ${item?.clientName}</li>
-            <li> Client Email: ${item?.clientEmail} </li>
-            <li>Client Phone: ${item?.clientPhone} </li>
-          </ul>
-        `
+    `
+      <h3 style='color:black'>
+        Hi ${item?.clientName}
+      </h3>
+      <p  style='color:black'>
+        Your event has been confirmed for: ${item?.startDate} at ${item?.startTime}, in the location: <b style="color:teal">${item?.locationName}</b>
+      </p>
+      <h5>
+        <b style="color:teal">Service Information:</b>
+      </h5>
+      <ul style='color:black'>
+        <li>PAX#: ${item?.headcount?.toddlers + item?.headcount?.adults}</li>
+        <li>Location: ${item?.locationName}</li>
+        <li>Date: ${item?.startDate}</li>
+        <li>Menu Items: ${item?.menuName}</li>
+        <li>Staff: ${item?.staff}</li>
+      </ul>
+      <h5>
+        <b style="color:teal">Client Information:</b>
+      </h5>
+      <ul style='color:black'>
+        <li>Client Name: ${item?.clientName}</li>
+        <li> Client Email: ${item?.clientEmail} </li>
+        <li>Client Phone: ${item?.clientPhone} </li>
+      </ul>
+    `
   );
 };
 
