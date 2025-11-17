08c07d568a8ca79ef885bfd46f62df975eb998be
josemorales-g360 <Jose_Morales@cw.groups360.com>
2025-11-17 16:56:03 -0600

    Implement sharedJsonSender for email handling in route files

--- routes/mobula.route.js
+++ routes/mobula.route.js
@@ -1,48 +1,31 @@
-const _ = require('lodash');
 const { CONFIG_ENV } = require('../config');
 const { Router } = require('express');
-const { requestIsEmpty, anyEmailRejected } = require('../common/validations');
-const { createTransporter } = require('../email/transporter');
-const { mobulaCommonTemplate } = require('../email/template/mobula');
-const { SuccessResponseObject, ErrorResponseObject } = require('../common/http');
+const { ErrorResponseObject } = require('../common/http');
+const { hersoCommonTemplate } = require('../email/template/herso');
+const { sharedJsonSender } = require('../common/sharedJsonSender');
 
-const MOBULA_PROD_ORIGIN = 'https://mobulaestudio.com';
+const PROD_ORIGIN = 'https://mobulaestudio.com';
 const r = Router();
 
-const mobulaTransporter = createTransporter({
-  service: "Gmail",
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: CONFIG_ENV.MOBULA_SENDER_EMAIL,
-    pass: CONFIG_ENV.MOBULA_SENDER_PASSWORD,
-  },
-})
-
 r.post('/', async (req, res) => {
-  if (requestIsEmpty(req)) {
+  try {
+    await sharedJsonSender({
+      req: req,
+      res: res,
+      subject: 'Nuevo Email para mobula',
+      fromText: 'Un nuevo Email para mobula',
+      PROD_URL: PROD_ORIGIN,
+      fnTemplate: hersoCommonTemplate,
+      TO_EMAIL: CONFIG_ENV.MOBULA_TO_EMAIL,
+      SENDER_EMAIL: CONFIG_ENV.MOBULA_SENDER_EMAIL,
+      SENDER_PASSWORD: CONFIG_ENV.MOBULA_SENDER_PASSWORD,
+    })
+  } catch (e) {
+    console.error('Error on prepare the email', e);
     return res
-      .status(404)
-      .json(new ErrorResponseObject('The body or files are required'))
+      .status(401)
+      .json(new ErrorResponseObject('Error on prepare the email'));
   }
-
-  const template = mobulaCommonTemplate(req?.body)
-  const originName = req.get('origin')
-  const sendEmailResponse = await mobulaTransporter.sendMail({
-    from: `Un nuevo Email para mobula <${CONFIG_ENV.MOBULA_SENDER_EMAIL}>`,
-    to: originName === MOBULA_PROD_ORIGIN ? env.MOBULA_TO_EMAIL : 'jose.morales@hermosasoftware.io',
-    subject: 'Nuevo Email para mobula',
-    html: template
-  });
-
-  if (!anyEmailRejected(sendEmailResponse)) {
-    return res
-      .status(404)
-      .json(new ErrorResponseObject(`These emails were rejected ${sendEmailResponse?.rejected?.toString()}`))
-  }
-
-  return res.status(200).json(new SuccessResponseObject('Email send'))
 });
 
 module.exports = r;
